2d84085e64e8a7f1be30965681b7b2b6cf72f3ee
Oleksandr Hrishchuk <alexgrischuk@gmail.com>
2020-11-25 21:08:17 +0100

    extend path, enable dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 module.exports = {
-  purge: ['./pages/**/*.js'],
+  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'media', // 'media' or 'class'
   theme: {
+    extend: {
+      colors: {},
+    },
+  },
+  variants: {
     extend: {},
   },
-  variants: {},
   plugins: [],
 }
